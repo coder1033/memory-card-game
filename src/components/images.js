8e1c5ed035e5e88023624d9f8921d7128c961c93
Manan Jain <manan09mj@gmail.com>
2021-01-20 15:46:55 +0530

    import all the images for cards

---
+++ src/components/images.js
@@ -0,0 +1,37 @@
+import cherry from "./images/cherrybomb";
+import chomper from "./images/chomper"
+import fumeshroom from "./images/fumeshroom"
+import hypnoshroom from "./images/hypnoshroom"
+import kernelpult from "./images/kernelpult"
+import magnetshroom from "./images/magnetshroom"
+import melonpult from "./images/melonpult"
+import peapod from "./images/peapod"
+import peashooter from "./images/peashooter"
+import snowpea from "./images/snowpea"
+import sporeshroom from "./images/sporeshroom"
+import sunflower from "./images/sunflower"
+import tallnut from "./images/tallnut"
+import torchwood from "./images/torchwood"
+import wintermelon from "./images/wintermelon"
+
+
+
+const cards = {
+    cherry, 
+    chomper,
+    fumeshroom,
+    hypnoshroom,
+    kernelpult,
+    magnetshroom,
+    melonpult,
+    peapod,
+    peashooter,
+    snowpea,
+    sporeshroom,
+    sunflower,
+    tallnut,
+    torchwood,
+    wintermelon
+}
+
+export default cards;
