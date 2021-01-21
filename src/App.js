1889b3ce3964733dd5825f759f9c13a60794ff37
Manan Jain <manan09mj@gmail.com>
2021-01-21 23:44:02 +0530

    Here logic of project is implemented States added in App component are cards_url, cards, score, high_score, selected & game_over. cards_url is an object which contains all the urls of images cards is an array which contains all the keys of images score is the current score high_score is the high score of a specific sever selected is a object which contains the information of clicked cards gameover is true when a clicked card is again clicked
    useEffect callback for updating the highscore
    
    onClickCard function checks if the card is already clicked or not. If its clicked then game ends. If not then it will mark that image selected and updates the score.
    
    onRestart function updates the game_over state to true
    
    Various Imports -
    cardsId & cardsUrl - these function will return an array of whole numbers and an object of image urls
    reshuffle - this function is a used for reshuffling of cards array
    AllCards - this is a component for handling all the cards
    Header - Name, current score and highscore will be rendered from here
    GameOver - This component will mount when game ends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+import { cardsId, cardsUrl } from "./cards-data/cards-data.js";
+import reshuffle from "./reshuffle/reshuffle.js";
+import AllCards from "./components/all-cards.js";
+import Header from "./components/header.js";
+import GameOver from "./components/game-over.js"
 
-function App() {
+const App = () => {
+  const [cards_url] = useState(cardsUrl());
+  const [cards, setCards] = useState(cardsId());
+  const [score, setScore] = useState(0);
+  const [high_score, setHighScore] = useState(0);
+  const [selected, setSelected] = useState({});
+  const [game_over, setGameOver] = useState(false);
+
+  useEffect(
+    () => {
+      if (high_score < score) setHighScore(high_score + 1);
+    },
+    [score, high_score]
+  );
+
+  const onClickCard = (card) => {
+    if (selected[card]) {
+      setScore(0);
+      setSelected((prevSelected) => {
+        return {};
+      });
+      setGameOver(true);
+      return;
+    }
+
+    setSelected((prevSelected) => ({
+      ...prevSelected,
+      [card]: true,
+    }));
+
+    setScore(score + 1);
+
+    setCards((prevCards) => reshuffle([...prevCards]));
+  };
+
+  const onRestart = () => {
+    setGameOver(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {game_over && <GameOver onRestart={onRestart}/>}
+      <Header score={score} high_score={high_score}/>
+      <AllCards onClickCard={onClickCard} cards={cards} cards_url={cards_url} />
     </div>
   );
-}
+};
 
 export default App;
