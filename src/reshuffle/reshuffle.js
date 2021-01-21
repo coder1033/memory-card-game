61d7aa65e306a80c426739c01c8c50c76e62c5ba
Manan Jain <manan09mj@gmail.com>
2021-01-21 22:51:35 +0530

    Algorithm for shuffling of images

---
+++ src/reshuffle/reshuffle.js
@@ -0,0 +1,13 @@
+const reshuffle = (cards) => {
+    let n = cards.length, temp;
+    while(n){
+        let r = Math.floor(Math.random() * n--);
+        
+        temp = cards[r];
+        cards[r] = cards[n];
+        cards[n] = temp;
+    }
+    return cards
+};
+
+export default reshuffle;
